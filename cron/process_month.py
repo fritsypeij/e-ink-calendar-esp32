9d62aed66e6cfa59404da56906d772ca48c7960d
antnks <antnks@users.noreply.github.com>
2023-11-30 12:31:43 +0200

    Include also the last week of the previous month

--- cron/process_month.py
+++ cron/process_month.py
@@ -10,10 +10,14 @@ mytz = pytz.UTC
 input_file = open(sys.argv[1], "r")
 ical_events = Calendar().from_ical(input_file.read())
 
+# get the first day of the current month
 start_date = datetime.today().replace(day=1, hour=0, minute=0, second=0, microsecond=0)
-end_date = start_date + relativedelta(months=1) + relativedelta(weeks=1)
+
+# include one week of the previous and the next months
+end_date   = start_date + relativedelta(weeks=1) + relativedelta(months=1)
+start_date = start_date - relativedelta(weeks=1)
 start_date = mytz.localize(start_date)
-end_date = mytz.localize(end_date)
+end_date   = mytz.localize(end_date)
 
 month = dict()
 for event in ical_events.walk():
