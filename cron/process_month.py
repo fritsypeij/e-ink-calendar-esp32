c3decd33e51ffc1830fb26afc9f3eeac20cc0e46
antnks <antnks@users.noreply.github.com>
2023-11-30 12:50:57 +0200

    Improved event array building

--- cron/process_month.py
+++ cron/process_month.py
@@ -5,6 +5,12 @@ from dateutil.relativedelta import relativedelta
 import pytz
 import sys
 
+def append_event(idx, dt, ev):
+	global month
+	if idx not in month:
+		month[idx] = list()
+	month[idx].append([dt, ev])
+
 mytz = pytz.UTC
 
 input_file = open(sys.argv[1], "r")
@@ -22,25 +28,24 @@ end_date   = mytz.localize(end_date)
 month = dict()
 for event in ical_events.walk():
 
-	if event.name == "VEVENT":
+	if event.name != "VEVENT":
+		continue
 
-		cal_date = event.get('dtstart').dt
-		event_date = datetime.combine(cal_date, datetime.min.time())
-		if event_date.tzinfo == None:
-			event_date = mytz.localize(event_date)
+	cal_date = event.get('dtstart').dt
+	event_date = datetime.combine(cal_date, datetime.min.time())
+	if event_date.tzinfo == None:
+		event_date = mytz.localize(event_date)
 
-		if event_date > start_date and event_date < end_date:
+	if event_date > start_date and event_date < end_date:
+		dayindex = int(event_date.strftime('%Y%m%d'))
 
-			if event_date.day not in month:
-				month[event_date.day] = list()
-
-			# check for full day event
-			delta = event.get('dtend').dt - event.get('dtstart').dt
-			if delta.days >= 1:
-				for i in range(0, delta.days):
-					month[event_date.day + i].append([None, event.get('summary')])
-			else:
-				month[event_date.day].append([event_date, event.get('summary')])
+		# check for full day event
+		delta = event.get('dtend').dt - event.get('dtstart').dt
+		if delta.days >= 1:
+			for i in range(0, delta.days):
+				append_event(dayindex + i, None, event.get('summary'))
+		else:
+			append_event(dayindex, event_date, event.get('summary'))
 
 for day in month:
 	print(day, month[day])
