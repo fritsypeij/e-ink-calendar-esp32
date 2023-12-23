45a86f7d56e0153bed5b85df00ebf2bdc191c6a6
antnks <antnks@users.noreply.github.com>
2023-12-23 14:48:15 +0200

    Added color inversion

--- cron/pull_calendar.sh
+++ cron/pull_calendar.sh
@@ -17,7 +17,7 @@ wget -q -O "$ICAL_SAVE" "$ICAL_SECRET_URL"
 # parse and save only requsted month and the next one
 python3 process_month.py "$ICAL_SAVE" "$PNG_SAVE"
 
-convert "$PNG_SAVE" -crop 1304x984+124+42 "$PBM_TEMP"
+convert "$PNG_SAVE" -crop 1304x984+124+42 -negate "$PBM_TEMP"
 chunk=$((1304*984/8/2))
 cat <(echo -n $HEADER0) <(tail -n +3 "$PBM_TEMP" | dd bs=1 count=$chunk skip=0 2>/dev/null)      | nc -w 5 $EID_IP $EID_PORT
 cat <(echo -n $HEADER1) <(tail -n +3 "$PBM_TEMP" | dd bs=1 count=$chunk skip=$chunk 2>/dev/null) | nc -w 5 $EID_IP $EID_PORT
