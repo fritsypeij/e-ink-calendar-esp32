8ac8ad29fa7818280a01572b00fb092727cb9b40
antnks <antnks@users.noreply.github.com>
2024-02-11 13:11:15 +0200

    Added better logging for troubleshooting

--- cron/pull_calendar.sh
+++ cron/pull_calendar.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+log_error ()
+{
+	echo "$1"
+	log_info "$1"
+}
+
+log_info ()
+{
+	logger "$0: $1"
+}
+
 # get script's dir
 SCRIPT=`realpath $0`
 DIR=`dirname $SCRIPT`
@@ -8,6 +19,7 @@ source $DIR/.env
 # if .env does not override, use the script's dir
 if [ -z "$BASEDIR"]
 then
+	log_info "BASEDIR not defined, setting to $DIR"
 	BASEDIR=$DIR
 fi
 
@@ -29,46 +41,60 @@ RED_UP="EID2"
 RED_DN="EID3"
 CLEAR="EID9"
 
-# download calendar to a temp file
+log_info "download calendar to a temp file"
 wget -q -O "$ICAL_SAVE" "$ICAL_SECRET_URL"
 if [ ! -s "$ICAL_SAVE" ]
 then
-	echo "Unable to download the calendar"
+	log_error "Unable to download the calendar"
 	exit 1
 fi
 
-# parse and save only requsted month and the next one
+log_info "parse and save only requsted month and the next one"
 python3 process_month.py "$ICAL_SAVE" "$HTML_DOC"
+if [ $? != 0 ]
+then
+	log_error "python code failed"
+	exit 10
+fi
 
-# detect firefox
+log_info "detect firefox"
 firefox --version > /dev/null
 if [ $? != 0 ]
 then
-	echo "Unable to run headless firefox"
+	log_error "Unable to run headless firefox"
 	exit 2
 fi
+
+log_info "run firefox to make screenshot only"
 firefox --headless --screenshot "$PNG_SAVE" "file://$HTML_DOC" 1> /dev/null 2> /dev/null
+log_info "crop area that fits our screen"
 convert "$PNG_SAVE" -crop 1304x984+12+4 "$PNG_CROP"
 if [ $? != 0 ]
 then
-	echo "Unable to crop PNG"
+	log_error "Unable to crop PNG"
 	exit 3
 fi
 
-# convert to black
+log_info "extract black channel to $PBM_B_TEMP"
 convert "$PNG_CROP" -negate "$PBM_B_TEMP"
 
-# save only red color
+log_info "extract red channel to $PBM_R_TEMP"
 convert "$PNG_CROP" -fill white +opaque red "$PBM_R_TEMP.png"
 convert "$PBM_R_TEMP.png" -negate "$PBM_R_TEMP"
 
-# clear display
+log_info "clear display"
 cat <(echo -n $CLEAR) | nc -w 1 $EID_IP $EID_PORT
 sleep 30
 
 chunk=$((1304*984/8/2))
+log_info "sending black top"
 cat <(echo -n $BCK_UP) <(tail -n +3 "$PBM_B_TEMP" | dd bs=1 count=$chunk skip=0 2>/dev/null)      | nc -w 10 $EID_IP $EID_PORT
+log_info "sending black bottom"
 cat <(echo -n $BCK_DN) <(tail -n +3 "$PBM_B_TEMP" | dd bs=1 count=$chunk skip=$chunk 2>/dev/null) | nc -w 10 $EID_IP $EID_PORT
+log_info "sending red up"
 cat <(echo -n $RED_UP) <(tail -n +3 "$PBM_R_TEMP" | dd bs=1 count=$chunk skip=0 2>/dev/null)      | nc -w 10 $EID_IP $EID_PORT
+log_info "sending red down"
 cat <(echo -n $RED_DN) <(tail -n +3 "$PBM_R_TEMP" | dd bs=1 count=$chunk skip=$chunk 2>/dev/null) | nc -w 10 $EID_IP $EID_PORT
+
+log_info "done"
 
