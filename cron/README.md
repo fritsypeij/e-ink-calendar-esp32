d6d2eb431f7f2f0fe39217eecc0e75ca62bcfecd
Frits Ypeij <frits@ypeij.nl>
2024-04-10 03:25:30 +0200

    Modify readme and install script

--- cron/README.md
+++ cron/README.md
@@ -1,47 +1,91 @@
-# Prereqs
+# Docker (Dockerfile):
 
-For Ubuntu:
+This guide is for those who want to have a quick and easy setup. The following commands will create a docker container based on Alpine Linux and install the necessary packages.
 
+Create the `config.sh` file based on the provided `config.sh.sample` file. This file contains the necessary configuration for the calendar to work.
+
+Create a folder on your host machine and copy the following files into it:
+
+``` bash
+config.sh # Create this file based on config.sh.sample
+process_month.py
+pull_calendar.sh
+style.css
+template.shtml
+Dockerfile
 ```
+
+Grab your favorite terminal, create, start and connect to an Alpine docker container with the following commands:
+
+``` bash
+docker build -t calendar .
+docker run -d --restart=always -v /path/on/host:/app -e BASEDIR=/app -e TEMPDIR=/app/temp --name calendar calendar crond -f
+```
+
+# Docker (manually):
+
+Manually create a docker container based on Alpine Linux and install the necessary packages. This guide is for those who want to have more control over the installation process.
+
+Create the `config.sh` file based on the provided `config.sh.sample` file. This file contains the necessary configuration for the calendar to work.
+
+Create a folder on your host machine and copy the following files into it:
+
+``` bash
+config.sh # Create this file based on config.sh.sample
+process_month.py
+pull_calendar.sh
+style.css
+template.shtml
+```
+
+Grab your favorite terminal, create, start and connect an Alpine docker container with the following commands:
+
+``` bash
+docker run -d --restart=always -v /path/on/host:/app -e BASEDIR=/app -e TEMPDIR=/app/temp --name calendar alpine:latest crond -f
+docker exec -it calendar bin/ash
+```
+
+You're now connected to your new docker container. Run the following commands to install the necessary packages:
+
+``` bash
+apk update
+apk add nano wget python3 bash netcat-openbsd py3-pip imagemagick firefox
+pip3 install icalendar recurring_ical_events --break-system-packages
+```
+
+## Cron
+
+1) Open the crontab file:
+   ``` bash
+   crontab -e
+   ```
+2) Add the following line to run pull_calendar.sh every hour:
+   ``` bash
+   0 * * * * /app/pull_calendar.sh
+   ```
+   To update the calendar on other intervals, check out this [cron cheatsheet](https://devhints.io/cron).
+3) Save and exit (In nano, press `Ctrl+X`, then `Y`, then `Enter`).
+
+Alternatively, you can use Alpine's periodic cron folders inside `/etc/periodic/`. Add a bash script to one of the predefined folders that executes `/app/pull_calendar.sh`.
+
+Exit the container shell with `exit` and then restart the container with `docker restart calendar`.
+
+
+# Ubuntu:
+
+``` bash
 sudo apt install wget python3 python3-pip imagemagick netcat firefox language-pack-lt
 sudo pip3 install icalendar recurring_ical_events
 ```
 Note, Ubuntu may also aks you to run `sudo snap install firefox`, check terminal output.
 
-For Alpine:
+## Cron
 
-```
-apk update
-apk add wget python3 bash netcat-openbsd py3-pip imagemagick firefox
-```
+Run `crontab -e` to edit the cron file.
+Add these lines to update the display hourly:
 
-# Env file
-
-Create `.env` file with your real Google magic link and display's address, ex:
-
-```
-ICAL_SECRET_URL="https://calendar.google.com/calendar/ical/xxx%40group.calendar.google.com/private-xxx/basic.ics"
-EID_IP="192.168.1.1"
-EID_PORT="80"
+``` bash
+0 * * * * /bin/bash /path/to/pull_calendar.sh
 ```
 
-# Cron
-
-Add this line to your cron:
-
-```
-SHELL=/bin/bash
-0 0 * * * cd /path/to/git/e-ink-calendar-esp32/cron && source .env && ./pull_calendar.sh
-```
-
-# Docker
-
-Optionally, you can run script in docker.
-
-To build, run `build_docker.sh`.
-
-To run:
-
-```
-docker run -ti calendar
-```
+To update the calendar on other intervals, check out this [cron cheatsheet](https://devhints.io/cron).
--- cron/build_docker.sh
+++ cron/build_docker.sh
@@ -1,10 +1,218 @@
 #!/bin/bash -e
 
-source .env
+echo "This script will build the docker image and run the container."
+echo "You may need to run this script with elevated permissions (sudo)."
 
-docker build \
-	--build-arg ICAL_SECRET_URL="$ICAL_SECRET_URL" \
-	--build-arg EID_IP="$EID_IP" \
-	--build-arg EID_PORT="$EID_PORT" \
-	-t calendar .
+while true; do
+	read -p "Do you want to continue? (Y/n) " CONTINUE
+	if [ "$CONTINUE" == "n" ]; then
+		exit 0
+	elif [ "$CONTINUE" == "y" ] || [ "$CONTINUE" == "Y" ] || [ -z "$INPUT" ]; then
+		break
+	else
+		echo "Invalid input. Please enter y or n."
+	fi
+done
 
+echo ""
+
+skipcreateconfig=false
+# check if the config file exists
+if [ -f config.sh ]; then
+	source config.sh
+
+	echo "A config file already exists:"
+	echo ""
+	echo "--------------- config.sh ---------------"
+	echo ""
+	echo "ESP32 IP: $EID_IP"
+	echo "ESP32 Port: $EID_PORT"
+	echo "Calendar URLs:"
+	for url in "${ICAL_SECRET_URLS[@]}"; do
+		echo "  $url"
+	done
+	echo ""
+	echo "-----------------------------------------"
+	echo ""
+	while true; do
+		read -p "Do you want to continue with these settings? [Y/n]: " CONTINUE
+		if [ "$CONTINUE" == "n" ]; then
+			break
+		elif [ "$CONTINUE" == "y" ] || [ "$CONTINUE" == "Y" ] || [ -z "$INPUT" ]; then
+			echo "Using the existing config file."
+			skipcreateconfig=true
+			break
+		else
+			echo "Invalid input. Please enter y or n."
+		fi
+	done
+fi
+if [ "$skipcreateconfig" == false ]; then
+	echo "You will need to provide the IP address and port of the ESP32."
+	echo ""
+	while true; do
+		read -p "Enter the IP address: " EID_IP
+		if [[ $EID_IP =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+			IFS='.' read -ra IP <<< "$EID_IP"
+			for i in "${IP[@]}"; do
+				if [[ $i -lt 0 || $i -gt 255 ]]; then
+					echo "Invalid IP address. Please try again."
+					continue 2
+				fi
+			done
+			break
+		else
+			echo "Invalid IP address. Please try again."
+		fi
+	done
+
+	while true; do
+		read -p "Enter the web port: " EID_PORT
+		if [[ $EID_PORT =~ ^[0-9]+$ ]] && [ $EID_PORT -ge 1 ] && [ $EID_PORT -le 65535 ]; then
+			break
+		else
+			echo "Invalid port number. Please enter a number between 1 and 65535."
+		fi
+	done
+	echo ""
+	echo "GREAT! Now let's add one or multiple calendar URLs."
+	echo "Just press enter to add another. Keep empty to finish."
+	echo ""
+
+	while true; do
+		ICAL_SECRET_URLS=()
+		while true; do
+			read -p "Enter a calendar URL: " URL
+			if [ -z "$URL" ]; then
+				break
+			elif [[ "$URL" == https://* ]]; then
+				ICAL_SECRET_URLS+=("$URL")
+			elif [[ "$URL" == webcal://* ]]; then
+				echo "Replacing webcal:// with https://"
+				URL=${URL/webcal:\/\//https:\/\/}
+				ICAL_SECRET_URLS+=("$URL")
+			else
+				echo "Invalid URL. The URL should start with https://"
+				continue
+			fi
+		done
+		if [ ${#ICAL_SECRET_URLS[@]} -eq 0 ]; then
+			echo "No URLs provided. Please enter at least one URL."
+		else
+			break
+		fi
+	done
+
+	# write the config file
+	echo "ICAL_SECRET_URLS=(" > config.sh
+	for url in "${ICAL_SECRET_URLS[@]}"; do
+		echo "  \"$url\"" >> config.sh
+	done
+	echo ")" >> config.sh
+	echo "EID_IP=\"$EID_IP\"" >> config.sh
+	echo "EID_PORT=\"$EID_PORT\"" >> config.sh
+
+	echo ""
+	echo "Config file created with:"
+	echo ""
+	echo "--------------- config.sh ---------------"
+	echo ""
+	echo "ESP32 IP: $EID_IP"
+	echo "ESP32 Port: $EID_PORT"
+	echo "Calendar URLs:"
+	for url in "${ICAL_SECRET_URLS[@]}"; do
+		echo "  $url"
+	done
+	echo ""
+	echo "-----------------------------------------"
+	echo ""
+fi
+
+echo ""
+
+if ! command -v docker &> /dev/null; then
+	echo "From this point on you need to run this script with elevated permissions (sudo)."
+	echo "Sudo is needed to install (apt-get) and run Docker."
+	while true; do
+		read -p "Do you want to install Docker? [Y/n]: " CONTINUE
+		if [ "$CONTINUE" == "n" ]; then
+			exit 0
+		elif [ "$CONTINUE" == "y" ] || [ "$CONTINUE" == "Y" ] || [ -z "$INPUT" ]; then
+			break
+		else
+			echo "Invalid input. Please enter y or n."
+		fi
+	done
+
+	echo ""
+	echo "Installing Docker..."
+	echo ""
+
+	sudo apt-get update
+	sudo apt-get install -y docker.io
+
+	echo ""
+	echo "Docker installed."
+	echo ""
+
+	while true; do
+		read -p "Do you want to enable and start the Docker service? [Y/n]: " CONTINUE
+		if [ "$CONTINUE" == "n" ]; then
+			break
+		elif [ "$CONTINUE" == "y" ] || [ "$CONTINUE" == "Y" ] || [ -z "$INPUT" ]; then
+			sudo systemctl enable docker
+			sudo systemctl start docker
+			break
+		else
+			echo "Invalid input. Please enter y or n."
+		fi
+	done
+else
+	echo "Docker is installed."
+	echo ""
+	echo "From this point on you need to run this script with elevated permissions (sudo)."
+	echo "Sudo is needed to run Docker."
+fi
+
+echo ""
+echo "Going to build the docker image..."
+echo ""
+
+sudo docker build -t calendar .
+
+echo ""
+echo "Building the calendar container done."
+while true; do
+	read -p "Do you want to start the calendar container? [Y/n]: " CONTINUE
+	if [ "$CONTINUE" == "n" ]; then
+		exit 0
+	elif [ "$CONTINUE" == "y" ] || [ "$CONTINUE" == "Y" ] || [ -z "$INPUT" ]; then
+		break
+	else
+		echo "Invalid input. Please enter y or n."
+	fi
+done
+echo ""
+echo "Running the calendar container..."
+echo ""
+sudo docker run -d --name calendar calendar
+echo ""
+echo "Calendar container running! You should see the display flicker soon(tm)."
+while true; do
+	read -p "Do you want to show the logs while starting the container for the first time? [Y/n]: " CONTINUE
+	if [ "$CONTINUE" == "n" ]; then
+		echo "You can see the logs at any time by running 'sudo docker logs -f calendar'"
+		echo ""
+		echo "goodbye!"
+		exit 0
+	elif [ "$CONTINUE" == "y" ] || [ "$CONTINUE" == "Y" ] || [ -z "$INPUT" ]; then
+		echo "You can stop viewing the logs by pressing Ctrl+C"
+		echo "Showing logs..."
+		echo ""
+		echo ""
+		sudo docker logs -f calendar
+		break
+	else
+		echo "Invalid input. Please enter y or n."
+	fi
+done
